a5f0f0c7c7ceecbdf8a4778b1f228d4c09e01e12
vochoaa <vochoaa@eafit.edu.co>
2023-08-30 21:07:05 -0500

    commit

--- Reto 2/gateway/src/app.py
+++ Reto 2/gateway/src/app.py
@@ -28,7 +28,7 @@ def list_files():
         return jsonify({"files": [file.filename for file in response.files]})
     
 
-app.route('/search-files')
+app.route('/buscar')
 def search_files():
     query = request.args.get('query')
 
